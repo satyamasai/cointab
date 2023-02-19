679f026b68e5adb2843a92e3613090fc8eb88e95
satyam gupta <stymngrya@gmail.com>
2023-02-20 03:00:31 +0530

    all functions working

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,6 +1,6 @@
-
+require("dotenv").config()
  const mongoose = require("mongoose")
-const connection = mongoose.connect("mongodb+srv://satyam1516:161996@cluster0.ubnagby.mongodb.net/cointab_database?retryWrites=true&w=majority")
+const connection = mongoose.connect(process.env.MONGOURL)
 module.exports={
     connection
 }
--- backend/Routes/userRoute.js
+++ backend/Routes/userRoute.js
@@ -3,46 +3,68 @@ const axios = require("axios");
 const { userModel } = require("../Models/userModel");
 const userController = Router();
 
+
+
 userController.get("/get-user", async (req, res) => {
   console.log(req.query,"query")
+  const {city,gender,state,page}= req.query
+ 
+  const query = {}
+
+  if(gender){
+    query.gender=gender
+  }
+  if(city){
+    query["location.city"] = city
+  }
+  if(state){
+    query["location.state"] = state
+
+  }
+  if(page){
+    query
+  }
   // console.log(req.body)  
-  const userData= await userModel.find();
+  const userData= await userModel.find(query).skip(page).limit(10);
   // console.log(userData)
   res.send({userData})
 
 });
-// for male users
-
-userController.get("/get-user/male", async (req, res) => {
-    console.log(req.body)  
-    const userData= await userModel.find({"gender":"male"});
-    console.log(userData)
-    res.send({userData})
-  
-  });
 
 
-  // for filter female user
-  userController.get("/get-user/female", async (req, res) => {
-    console.log(req.body)  
-    const userData= await userModel.find({"gender":"female"});
-    console.log(userData)
-    res.send({userData})
-  
-  });
+
+ 
 
 // add data to mongodb
 userController.get("/userdb",async (req,res)=>{
     await axios
     .get("https://randomuser.me/api/?results=50")
     .then(async (result) => {
-        console.log("RES", result.data.results);
+        // console.log("RES", result.data.results);
         await userModel.insertMany(result.data.results)
-        res.send({"msg":"user added successfully to db"})
+        res.send({"msg":"user added successfully to db","result":result.data.results})
     })
     .catch((err) => {
       console.log("err", err);
     });
+
+
+})
+
+
+// for city state name only
+
+userController.get("/userdb1",async (req,res)=>{
+  await axios
+  .get("https://randomuser.me/api/?results=50")
+  .then(async (result) => {
+      // console.log("RES", result.data.results);
+      // await userModel.insertMany(result.data.results)
+      res.send({"msg":"user added successfully to db","result":result.data.results})
+  })
+  .catch((err) => {
+    console.log("err", err);
+  });
 
 
 })
